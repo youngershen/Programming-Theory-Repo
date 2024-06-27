b403bff3bf9c7e92ac31dfedf2ec3cb0994b1a15
Younger Shen <shenyangang@163.com>
2024-06-27 15:30:13 +0800

    save code

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class Animal : MonoBehaviour
+{
+
+    public string Name { get; protected set; }
+    public int Identity { get; protected set; }
+
+
+    public abstract void Speak();
+
+
+    public virtual void Walk()
+    {
+        const int speed = 10;
+        var text = Name + " is walking at speed " + speed;
+        Debug.Log(text);
+    }
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : Animal
+{
+    public override void Speak()
+    {
+        var text = "My name is" + Name;
+        var text2 = "My identity is " + Identity;
+        const string text3 = "And i am a cat";
+
+        Debug.Log(text);
+        Debug.Log(text2);
+        Debug.Log(text3);
+    }
+
+    public override void Walk()
+    {
+        const int speed = 20;
+        var text = Name + " is walking at speed " + speed;
+        Debug.Log(text);
+    }
+    
+    
+    private void Start()
+    {
+        Name = "Cat";
+        Identity = 123456;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
